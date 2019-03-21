2a1be1f57d82af667fe64e621f42d58919cb91ae
getynge <sparklytarnish+git@protonmail.com>
2019-03-20 22:44:19 -0400

    further cleanup, reduced tab size

--- CompactGuilds.plugin.js
+++ CompactGuilds.plugin.js
@@ -255,48 +255,48 @@ class CompactGuilds {
 		// ???
 		var settings = this.loadSettings();
 		var html = `<h3>Settings Panel</h3><br>
-	 Animation speed <br>
+		Animation speed <br>
 		<input id='hg_animspeed' type='number'value=${settings.animspeed}> milliseconds<br><br>
 
-	 Animation style<br>
-	 <select id='hg_animstyle'>`
+		Animation style<br>
+		<select id='hg_animstyle'>`
 		var a = ['Linear', 'Ease', 'Ease-in', 'Ease-out', 'Ease-in-out'];
 		for (var i = 0; i < a.length; i++) {
 			html += "<option value='" + a[i] + "'" + ((settings.animstyle == a[i]) ? "selected" : "") + ">" + a[i];
 		}
 		`</select><br><br>
 
-	 Show distance<br>
-	 <input id='hg_show' type='number' value=${settings.show}> pixels<br><br>
+		 Show distance<br>
+	 	<input id='hg_show' type='number' value=${settings.show}> pixels<br><br>
 
-	 Hide distance<br>;
+	 	Hide distance<br>;
 		<input id='hg_hide' type='number' value=${settings.hide}> pixels<br><br>
 
-	 Max window size to enable compact guilds<br>
-	 <input id='hg_activeWidth' type='number' value=${settings.activeWidth} > pixels<br><br>
+	 	Max window size to enable compact guilds<br>
+	 	<input id='hg_activeWidth' type='number' value=${settings.activeWidth} > pixels<br><br>
 	
-	 <input type='checkbox' id='hg_always' ${(settings.always) ? 'checked' : ''} >
-	 Enable guild hiding always<br>
+	 	<input type='checkbox' id='hg_always' ${(settings.always) ? 'checked' : ''} >
+	 	Enable guild hiding always<br>
 
 
 
-	 <input type='checkbox' id='hg_enableminsize' ${(settings.overrideMins) ? 'checked' : ''}>
-	 Override default window minimum sizes (restart discord when toggling off)<br>
-	 Width:  <input id='hg_minWidth'  type='number' value=${settings.minWidth}> pixels<br>
-	 Height: <input id='hg_minHeight' type='number' value= ${settings.minHeight} > pixels<br><br>
+	 	<input type='checkbox' id='hg_enableminsize' ${(settings.overrideMins) ? 'checked' : ''}>
+	 	Override default window minimum sizes (restart discord when toggling off)<br>
+	 	Width:  <input id='hg_minWidth'  type='number' value=${settings.minWidth}> pixels<br>
+	 	Height: <input id='hg_minHeight' type='number' value= ${settings.minHeight} > pixels<br><br>
 
 
-	 <input type='checkbox' id='hg_trim'
-	 (settings.trim) ?  checked> : >
-	 Make channels thinner<br>
+	 	<input type='checkbox' id='hg_trim'
+	 	(settings.trim) ?  checked> : >
+	 	Make channels thinner<br>
 
-	 <input type='checkbox' id='hg_mobilefy' ${(settings.mobilefy) ? 'checked' : ''} >
-	 Hide channels too<br>
+	 	<input type='checkbox' id='hg_mobilefy' ${(settings.mobilefy) ? 'checked' : ''} >
+	 	Hide channels too<br>
 
-	<br><button onclick=BdApi.getPlugin('${this.getName()}').saveSettings(this)>Save and apply</button>
-	<button onclick=BdApi.getPlugin('${this.getName()}').resetSettings(this)>Reset settings</button> <br><br>
+		<br><button onclick=BdApi.getPlugin('${this.getName()}').saveSettings(this)>Save and apply</button>
+		<button onclick=BdApi.getPlugin('${this.getName()}').resetSettings(this)>Reset settings</button> <br><br>
 
-	 If your hide distance is too low you might not be able to access the settings panel. Use CTRL+COMMA (,) if this happens.`;
+	 	If your hide distance is too low you might not be able to access the settings panel. Use CTRL+COMMA (,) if this happens.`;
 		return html;
 	};
 }
